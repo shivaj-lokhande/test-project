602912238ae581ff3fc911c708a6432cadc3b2bb
shivaj-lokhande <81822094+shivaj-lokhande@users.noreply.github.com>
2021-05-20 19:39:16 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp() {
+  echo 'we are building app..(using groovy script)'
+}
+
+def testApp() {
+  echo 'we are testing app..(using groovy script)'
+}
+
+def deployApp() {
+  echo 'we are deploying  app..(using groovy script)'
+}
+
+return this
